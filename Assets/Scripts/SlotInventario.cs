933141eb7d2ff5b92bec5001065ce75f24528939
GabrielMaleson <167476506+GabrielMaleson@users.noreply.github.com>
2026-02-16 17:30:20 -0300

    aaaaaaaaa

---
+++ Assets/Scripts/SlotInventario.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[System.Serializable] //OBRIGATÓRIO para visualizar no Inspector
+public class SlotInventario
+{
+    public DadosItem dadosDoItem;
+    public int quantidade;
+
+    //Construtor
+    public SlotInventario(DadosItem item, int qtd)
+    {
+        dadosDoItem = item;
+        quantidade = qtd;
+    }
+
+    public void AdicionarQuantidade(int qtd)
+    {
+        quantidade += qtd;
+    }
+
+    public void SubtrairQuantidade(int qtd)
+    {
+        quantidade -= qtd;
+    }
+}
