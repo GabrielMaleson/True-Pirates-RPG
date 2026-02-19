98e4e0dce2d8d4d8e030bec918b3c77180facd65
GabrielMaleson <167476506+GabrielMaleson@users.noreply.github.com>
2026-02-19 18:24:34 -0300

    adicionando ultimas receitas

--- Assets/Scripts/TesteInventario.cs
+++ Assets/Scripts/TesteInventario.cs
@@ -5,9 +5,15 @@ public class TesteInventario : MonoBehaviour
     public SistemaInventario inventario;
 
     public DadosItem espada;
+    public DadosItem escudo;
+    public DadosItem livro;
+    public DadosItem pote;
 
     private void Start()
     {
         inventario.AdicionarItem(espada, 1);
+        inventario.AdicionarItem(escudo, 1);
+        inventario.AdicionarItem(livro, 1);
+        inventario.AdicionarItem(pote, 1);
     }
 }
